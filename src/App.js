8423782551a073e31380d43b4c7bd162842b08a6
runzw <runzw0817@gmail.com>
2021-04-10 14:44:30 -0700

    add layout

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import MapContainer from './Map/MapContainer';
 import Post from './post/Post';
 
 function App() {
+  //localStorage.clear()
+  //localStorage.setItem("wreckbeach",JSON.stringify(photos))  
   return (
     <BrowserRouter>
       <Switch>
